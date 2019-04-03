8d08971f7db34f06176bc9c3405c52f0e1ef55be
Ilana LEPPERT <ilana.leppert@mcgill.ca>
2019-04-03 14:14:41 -0400

    read dicom information directly and create an info.txt file with the acqusition parameters

--- unshuffle_diff.m
+++ unshuffle_diff.m
@@ -1,51 +1,95 @@
-function unshuffle_diff(file,shift,spacing,TR,numavg,diffdirs,minTI,mode,mb)
-[HDR,VOL] = niak_read_nifti(file);
- 
-%mode='interleave';
-% shift=11;
-% spacing=99.98; %[ms] TI spacing as output by the sequence
-% TR=5700;
-% diffdirs=31; %include b=0
-% numavg=5;
-% mb=3; multi-band factor
+function unshuffle_diff(img,dcm,output)
+%%%%
+%%  Unshuffling IR-diff data
+%%%%%%%
 %
+% numavg = (Nslices/MB)/Nshift
+% Nslices/MB = excitations per inversion
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% INPUTS
+% img : shuffled nifti file, from the scanner
+% dcm : single dicom file to read timing info from
+% output: name of unshuffled nii file
+%
+% OUTPUTS:
+% unsuffled nii file
+% TI.txt : list of inversion times for each slice
+% info.txt : list of useful acquisition parameters
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+[HDR,VOL] = niak_read_nifti(img);
+
+% NOTE: only works for transverse right now
+
 %dim='sag';
 dim='tra';
 
 [x,y,z,t]=size(VOL);
 
-switch mode
+%read dicom data:
+[ header ] = readParamsFromDicom_irl(dcm);
+switch header.SliceOrderMode
+    case 1
+        SliceOrderMode = 'ascending';
+    case 4
+        SliceOrderMode = 'interleaved';
+    otherwise
+        error('Unknown slice order mode');
+end
+
+mb = header.MultiBandFactor;
+
+switch SliceOrderMode
     case{'interleaved'}
-        if mod(z,2)==0 %even # of slices
-          sidx = [2:2:z,1:2:z];%slice index
+        if mb>1
+            if mod(z/mb,2)~=0 %odd #slices within a block
+                sidx = [1:2:z,2:2:z];%slice index
+            else %% even #slices in each MB block
+                if (rem(z/mb, 2) == 0) && (z >= 6) % this is a special case to prevent x-talk
+                    sidx_gr1 = [2:2:z/mb, 1:2:z/mb];
+                    z_ = z/mb ;% this shift happens within a group
+                    % even slices are in the first half of SliceOrder, odd slices in the last half SliceOrder
+                    % With slices in this order, even SMS factors will generate cross talk if there is more than 1 repetition
+                    % This is how siemens reorders the lines
+                    %                 % last line of even number slices moves to middle of group
+                    sidx_gr1(1:(z_/2)) =      sidx_gr1([1:floor(z_/4)    (z_/2) (floor(z_/4) + 1):(z_/2 - 1)]            );
+                    % first line of odd number slices moves to middle of the group
+                    sidx_gr1((z_/2 + 1):end) = sidx_gr1([2:(floor(z_/4)+1)     1      (floor(z_/4) + 2):(z_/2)    ] + z_/2);
+                    %now create as many rows as the MB factor, i.e. each
+                    %column corresponds to slices acquired at the same time
+                    sidx=sidx_gr1;
+                    for m=2:mb
+                        sidx = [sidx;sidx_gr1+ (m-1)*z/mb];
+                    end
+                else
+                    sidx=sidx_gr1;
+                    sidx_gr1 = [2:2:z/mb, 1:2:z/mb];
+                    for m=2:mb
+                        sidx = [sidx;sidx_gr1+ (m-1)*z/mb];
+                    end
+                end
+            end
         else
-           sidx = [1:2:z,2:2:z];%slice index
-        end
-    case{'interleaved-mb'}
-        if mod(z/mb,2)~=0 %odd #slices within a block
-            %sidx = [1:2:z,2:2:z];%slice index
-        else %% even #slices
-            sidx_gr1 = [2:2:z/2, 1:2:z/2];
-            %sidx_gr2 = [z/2+2:2:z, z/2+1:2:z];
-            %sidx = [sidx_gr1;sidx_gr2];
-            if (rem(mb, 2) == 0) && (z >= 6)
-                z_ = z/mb ;% this shift happens within a group
-                % even slices are in the first half of SliceOrder, odd slices in the last half SliceOrder
-                % With slices in this order, even SMS factors will generate cross talk if there is more than 1 repetition
-                % This is how siemens reorders the lines
-%                 % last line of even number slices moves to middle of group
-                sidx_gr1(1:(z_/2)) =      sidx_gr1([1:floor(z_/4)    (z_/2) (floor(z_/4) + 1):(z_/2 - 1)]            );
-                % first line of odd number slices moves to middle of the group
-                sidx_gr1((z_/2 + 1):end) = sidx_gr1([2:(floor(z_/4)+1)     1      (floor(z_/4) + 2):(z_/2)    ] + z_/2);
-                sidx = [sidx_gr1;sidx_gr1+z/2];
+            if mod(z,2)==0 %even # of slices
+                sidx = [2:2:z,1:2:z];%slice index
+            else
+                sidx = [1:2:z,2:2:z];%slice index
             end
         end
     case{'ascending'}
         sidx = 1:z;%slice index
 end
 
-%TI times
-ti=minTI:spacing:TR;
+% Other parameters we need for unshuffling
+shift = header.MIEPIShiftPerInv; %shift factor
+numavg = header.Repetitions +1; %number of averages, have to add 1, total number of runs
+TR = header.TR; % repetition time
+spacing = header.MIEPISliceSpacing; % time between the acquisition of 2 slices
+minTI = header.TI; % 1st inversion time
+diffdirs = header.DiffusionDirections+1; % we have to add 1 here because there's a b=0;
+
+%TI times in ms
+ti=(minTI:spacing:TR)./1000; %header is in us
 TI=ti(1:z/mb);
 
 % need to flip in z dimension if sagittal
@@ -75,7 +119,7 @@ else
     for i=1:numavg-1
         ind=[ind; circshift(sidx,-shift*i)]; %index of slices for each average
         %ind1=[ind1; circshift(sidx1,-shift*i)]; %index of slices for each average
-        %ind2=[ind2; circshift(sidx2,-shift*i)]; %index of slices for each average       
+        %ind2=[ind2; circshift(sidx2,-shift*i)]; %index of slices for each average
     end
 end
 %ind=cat(2,ind1,ind2);
@@ -103,10 +147,19 @@ for d=1:diffdirs
 end
 
 hdr=HDR;
-name=strcat('unshuffle_diff_',file);
+name=strcat(output);
 hdr.file_name=name;
 niak_write_nifti(hdr,v);
 save('TI.txt', 'TIm', '-ASCII');
 save('TRa.txt', 'TRa', '-ASCII');
+%some other useful output info
+fileID = fopen('info.txt','w');
+fprintf(fileID,'%s %5d\n','shift factor = ',shift);
+fprintf(fileID,'%s %5d\n','SMS = ',mb);
+fprintf(fileID,'%s %5d\n','numavg = ',numavg);
+fprintf(fileID,'%s %5d\n','TR(ms) = ',TR/1000);
+fprintf(fileID,'%s %5d\n','TI spacing(ms) = ',spacing/1000);
+fprintf(fileID,'%s %5d\n','Minimum TI(ms) = ',minTI/1000);
+fprintf(fileID,'%s %5d\n','Diffusion directions= ',diffdirs);
+fclose(fileID);
 
- 
